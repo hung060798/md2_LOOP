d8ab0111c7730b575b93745f7dcf5ec5ed3a97f3
hung060798 <hungpppp122gmail.com>
2021-06-08 08:48:11 +0700

    tinhLaiXuat

---
+++ src/tinhLaiXuat.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class tinhLaiXuat {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month =1;
+        double interestRate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap so tien gui:");
+        money = input.nextDouble();
+        System.out.println("Nhap so thang gui:");
+        month = input.nextInt();
+        System.out.println("Nhap lai xuat:");
+        interestRate = input.nextDouble();
+        double totalInterest =0;
+        for (int i=0; i<month;i++) {
+            totalInterest += money*(interestRate/100)/12*month;
+        }
+        System.out.println("Tong lai xuat la: " +totalInterest);
+    }
+}
