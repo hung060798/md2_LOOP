828fc6552e4e6850275ed3ac35bf9f0c7c2733ae
hung060798 <hungpppp122gmail.com>
2021-06-08 11:32:31 +0700

    second

--- src/soNguyenTo_100.java
+++ src/soNguyenTo_100.java
@@ -1,10 +1,21 @@
 
 public class soNguyenTo_100 {
     public static void main(String[] args) {
-        for (int i=2; i<100;i++){
-
+            int N=2;
+            while(N < 100){
+                boolean check = false;
+                for (int i =2; i<=Math.sqrt(N); i++){
+                    if (N % i == 0){
+                        check = true;
+                        break;
+                    }
+                }
+                if(check == false)
+                    System.out.println(N);
+                N++;
             }
-        }
 
     }
+}
+
 
